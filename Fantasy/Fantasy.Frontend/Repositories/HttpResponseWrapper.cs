f9ce6a597b203a59b28d1c347717f9df73a9f310
edwin.duque <edwin.duque@redarbor.csccsl.com>
2024-09-05 22:28:00 -0500

    Generic List

---
+++ Fantasy/Fantasy.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+
+namespace Fantasy.Frontend.Repositories;
+
+public class HttpResponseWrapper<T>
+{
+    public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+    {
+        Response = response;
+        Error = error;
+        HttpResponseMessage = httpResponseMessage;
+    }
+
+    public T? Response { get; set; }
+    public bool Error { get; set; }
+    public HttpResponseMessage HttpResponseMessage { get; }
+
+    public async Task<string?> GetErrorMessageAsync()
+    {
+        if (Response == null || HttpResponseMessage == null)
+        {
+            return null;
+        }
+
+        var statusCode = HttpResponseMessage.StatusCode;
+        if(statusCode == HttpStatusCode.NotFound)
+        {
+            return "Not Found";
+        }
+        if (statusCode == HttpStatusCode.BadRequest)
+        {
+            return await HttpResponseMessage.Content.ReadAsStringAsync();
+        }
+        if(statusCode == HttpStatusCode.Unauthorized)
+        {
+            return "Unauthorized";
+        }
+        if (statusCode == HttpStatusCode.Forbidden)
+        {
+            return "Forbidden";
+        }
+
+        return "Unexpected error";
+    }
+}
